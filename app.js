8fcbb42d489024785885857226204b6191416079
Trevor <trevorwerner@hotmail.com>
2021-09-05 06:07:18 -0400

    Rendering layout.pug, added app.js helper, removed comments

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const { projects } = require('./data.json')
+
+app.set('view engine', 'pug');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+   res.locals.data = projects;
+   res.render('index');
+});
+
+app.get('/about', (req, res) => {
+   res.render('about');
+});
+
+const getProjectData = projectId => projects[projectId];
+
+app.get('/project/:id', (req, res) => {
+   const { id } = req.params,
+   projectData = getProjectData(id);
+   res.render('project', { projectData });
+});
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`)
