51e7b8f7b6acabaeb8eb0bfa867a5af178458ff8
Trevor <trevorwerner@hotmail.com>
2021-09-08 06:50:44 -0400

    Added custom styling and error handling

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Portfolio site built using Node and Express
 
 ## Technologies used: 
 HTML, CSS, JavaScript, Node, Express, Pug
+
+### CSS
+Using FontAwesome to produce icons 
+Changed font to 'Architects Daughter' and 'Timmana'
+Added scaling to project thumbnail hover 
+
+### Custom 404, Server Error templates
+Added custom style templates for error rendering
+* 'page-not-found' and 'server' extend from 'error.pug'
--- app.js
+++ app.js
@@ -21,19 +21,38 @@ const getProjectData = projectId => projects[projectId];
 app.get('/project/:id', (req, res) => {
    const { id } = req.params,
    projectData = getProjectData(id);
-   res.render('project', { projectData });
+   if (projectData) {
+      res.render('project', { projectData });
+   } else {
+      const err = new Error();
+      err.status = 404;
+      err.message = `You chose a project that doesn't exist (yet)`;
+      res.render('page-not-found', { err })
+   }
 });
 
-app.use((req, res, next) => {
-   const err = new Error("Not Found");
-   err.status = 404;
+/* Used for testing potential server errors */
+app.use('/error', (req, res, next) => {
+   const err = new Error();
+   err.status = 500;
+   err.message = `There's an issue on our end`;
    next(err);
 });
 
+/* 404 Error Handler for non-existent routes */
+app.use((req, res, next) => {
+   const err = new Error(`It's 2021... that doesn't exist yet`)
+   res.status(404).render('page-not-found', { err });
+});
+
+/* Global Error Handler */
 app.use((err, req, res, next) => {
-   res.locals.error = err;
-   res.status(err.status);
-   res.render('error');
+   if (err.status === 404) {
+      res.status(err.status).render('not-found', { err });
+   } else {
+      err.message = err.message || 'Whoops... looks like an issue on the server';
+      res.status(err.status || 500).render('server', { err });
+   }
 });
 
 app.listen(port, () => {
